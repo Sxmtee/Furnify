e5ee315458b3fc350270c4d800c2860053c70740
sxmtee <somtoo76@gmail.com>
2025-01-30 09:07:53 +0100

    Initial Commit

---
+++ Furnify/FurnifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FurnifyApp.swift
+//  Furnify
+//
+//  Created by mac on 30/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct FurnifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
